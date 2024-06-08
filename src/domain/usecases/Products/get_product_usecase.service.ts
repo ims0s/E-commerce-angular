55ef04a75cd8363a6f4aca75b4c58ede9c95386a
mahmoudPc <sorour2001@gmail.com>
2024-06-08 18:12:00 +0300

    get product usecase

--- src/domain/usecases/Products/get_product_usecase.service.ts
+++ src/domain/usecases/Products/get_product_usecase.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@angular/core';
+import { IProduct } from 'src/data/datasource/data_model/product';
+import { IProductRepo } from 'src/data/repositries/repo_interfaces/IProductsRepo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Get_product_usecaseService {
 
-constructor() { }
+constructor(
+  private _product_repo : IProductRepo
+) { }
 
+
+  async execute(product_id:string):Promise<IProduct>{
+    let result = await this._product_repo.getProduct(product_id);
+
+    return result;
+  }
 }
