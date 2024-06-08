f066fbbf3188bcdb9b6c9e6c24664e40212d5353
mahmoudPc <sorour2001@gmail.com>
2024-06-08 18:13:50 +0300

    fixing styling in homePage , product details page finished

--- src/presenetation/pages/ProductDetailsPage/ProductDetailsPage.component.ts
+++ src/presenetation/pages/ProductDetailsPage/ProductDetailsPage.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/data/datasource/data_model/product';
+import { Get_product_usecaseService } from 'src/domain/usecases/Products/get_product_usecase.service';
 
 @Component({
   selector: 'app-ProductDetailsPage',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsPageComponent implements OnInit {
 
-  constructor() { }
-
+  product:IProduct={
+    _id:"",
+    imageCover:"",
+    price:0,
+    title:"",
+    ratingsAverage:0,
+    subcategory:[
+      {
+        name:""
+      }
+    ],
+    category:{
+      name:""
+    }
+  }
+  
+  constructor(
+    private get_product_useCase$ : Get_product_usecaseService,
+    private route : ActivatedRoute
+  ) {
+    this.getProductData(this.route.snapshot.params["productId"]);
+  }
+  
   ngOnInit() {
+  }
+
+  getProductData(id:string){
+    this.get_product_useCase$.execute(id).then(data => {
+      this.product=data;
+    })
   }
 
 }
